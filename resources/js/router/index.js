6fcb3ad7b6ae1900bc769c787f11373dfbfc6a1e
Faby-Baby <fabianbertrand94@gmail.com>
2024-04-08 21:26:20 -0400

    minor tweaks

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -4,7 +4,7 @@ import HomeView from "../Pages/HomeView.vue";
 import AddMovieView from '../Pages/AddMovieView.vue';
 import MoviesView from '../Pages/MoviesView.vue';
 import LoginView from '../Pages/LoginView.vue';
-import LogOut from '../components/LogOut.vue';
+import LogOut from '../Pages/LogOut.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
